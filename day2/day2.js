b20d5f957a07330bf8078b28dc8dc0cb31374bb6
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-12-03 13:21:05 -0600

    updated readme and comments

--- day2/day2.js
+++ day2/day2.js
@@ -30,6 +30,8 @@ In the example above, games 1, 2, and 5 would have been possible if the bag had 
 
 Determine which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the IDs of those games?
 
+my correct answer: 2101
+
 --- Part Two ---
 
 The Elf says they've stopped producing snow because they aren't getting any water! He isn't sure why the water stopped; however, he can show you how to get to the water source to check it out for yourself. It's just up ahead!
@@ -51,6 +53,9 @@ Game 5 needed no fewer than 6 red, 3 green, and 2 blue cubes in the bag.
 The power of a set of cubes is equal to the numbers of red, green, and blue cubes multiplied together. The power of the minimum set of cubes in game 1 is 48. In games 2-5 it was 12, 1560, 630, and 36, respectively. Adding up these five powers produces the sum 2286.
 
 For each game, find the minimum set of cubes that must have been present. What is the sum of the power of these sets?
+
+my correct answer: 58269
+
 */
   let input = await files.readAFile("input.txt");
   let total = 0;
