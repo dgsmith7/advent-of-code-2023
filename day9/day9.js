a050208a4d18ba2242536529f6baab49025116da
dgsmith7@hotmail.com <dgsmith7@hotmail.com>
2023-12-03 23:52:36 -0600

    completed day 4

---
+++ day9/day9.js
@@ -0,0 +1,12 @@
+"use strict";
+
+import * as files from "../files.js";
+
+(async () => {
+  /*
+
+*/
+  let input = await files.readAFile("input.txt");
+  let total = 0;
+  let lines = input.split(`\n`);
+})();
